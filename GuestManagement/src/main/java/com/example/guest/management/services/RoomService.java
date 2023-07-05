ec679ba36ef060d7076ab3a8bbef8ccf893aeefd
Muruganantham Jaisankar <jaimuruganantham@gmail.com>
2023-07-05 19:56:25 +0300

    Guest Registration with Room Number

--- GuestManagement/src/main/java/com/example/guest/management/services/RoomService.java
+++ GuestManagement/src/main/java/com/example/guest/management/services/RoomService.java
@@ -3,6 +3,8 @@ package com.example.guest.management.services;
 
 import com.example.guest.management.domain.Room;
 import com.example.guest.management.domain.RoomStatus;
+import com.example.guest.management.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,7 +14,8 @@ import java.util.List;
 public class RoomService {
 
     private List<Room> rooms;
-
+    @Autowired
+    private RoomRepository roomRepository;
     public RoomService() {
         // Initialize rooms with default values
         rooms = new ArrayList<>();
@@ -35,5 +38,6 @@ public class RoomService {
 
     public void save(Room room) {
         // No specific action required to save the room in this example
+        roomRepository.save(room);
     }
 }
